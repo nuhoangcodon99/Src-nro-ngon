8d3303bce005eb52ad78bd174cd1bf4f8e7dee1d
Nuhoang <tranvienvien51@gmail.com>
2024-07-03 19:40:07 +0700

    g
    g

---
+++ Scoure/src/main/java/nro/models/sieu_hang/SieuHangModel.java
@@ -0,0 +1,13 @@
+package nro.models.sieu_hang;
+
+import nro.models.player.Player;
+
+public class SieuHangModel {
+
+    public long player_id;
+    public int dame;
+    public int defend;
+    public int rank;
+    public String message;
+    public Player player = new Player();
+}
